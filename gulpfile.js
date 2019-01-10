d4ad7fc8d7668b3d3f80cd760e565ef6f9738d2c
Andrey <uytov1991@gmail.com>
2019-01-10 16:06:34 +0400

    Изменили галп

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,27 @@ var server = require("browser-sync").create();
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var del = require("del");
-var minify = require("csso");
+var minify = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var run = require("run-sequence");
+
+gulp.task("images", function() {
+    return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+        imagemin.optipng({optimizationLevel:3}),
+        imagemin.jpegtran({progressive:true}),
+        imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+})
 
 gulp.task ("style", function(){
     gulp.src("source/css/style.css")
+    .pipe(minify())
     .pipe(postcss([
         autoprefixer()
     ]))
-    .pipe(minify())
+    
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
@@ -30,14 +43,6 @@ gulp.task("clean", function () {
     return del("build");
 });
 
-gulp.task("build", function(done) {
-    run(
-        "clean",
-        "copy",
-        "style",
-        done
-    );
-});
 
 gulp.task("serve", function () {
     server.init({
@@ -47,4 +52,13 @@ gulp.task("serve", function () {
     gulp.watch("source/css/**/*.css", ["style"]);
     gulp.watch("source/*.html")
         .on("change", server.reload);
+});
+
+gulp.task("build", function(done) {
+    run(
+        "clean",
+        "copy",
+        "style",
+        done
+    );
 });
