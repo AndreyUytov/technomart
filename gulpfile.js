03d4bcccb614233cdc809aa9949aefd389d202e5
Andrey <uytov1991@gmail.com>
2019-01-11 11:33:04 +0400

    Новый

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task("images", function() {
     return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
         imagemin.optipng({optimizationLevel:3}),
-        imagemin.jpegtran({progressive:true}),
+        imagemin.jpegtran({progressive: true}),
         imagemin.svgo()
     ]))
     .pipe(gulp.dest("source/img"));
