c92b18824e84f68e679e312191cb250f1e7a377a
Andrey <uytov1991@gmail.com>
2019-01-28 14:05:01 +0400

    Доделал ползунок с диапозоном цен

--- src/js/scriptCatalog.js
+++ src/js/scriptCatalog.js
@@ -71,8 +71,11 @@ var rangeFilter = document.querySelector('.range-filter');
 
 var toggleMin = rangeFilter.querySelector('.toggle-min');
 var toggleMax = rangeFilter.querySelector('.toggle-max');
+var scale = rangeFilter.querySelector('.scale');
+var minPrice = rangeFilter.querySelector('.min-price input');
+var maxPrice = rangeFilter.querySelector('.max-price input');
 
-var mouseDownOn = function (button) {
+var mouseDownOnMintoggle = function (button) {
 	button.addEventListener('mousedown', function (evtDown){
 		evtDown.preventDefault();
 		var startCoord = {
@@ -86,12 +89,22 @@ var mouseDownOn = function (button) {
 			startCoord = {
 				x: moveEvt.clientX
 			};
-			toggleMin.style.left = (toggleMin.offsetLeft - shift.x) + 'px';
+				if ((toggleMin.offsetLeft - shift.x) >=20){
+					toggleMin.style.left = (toggleMin.offsetLeft - shift.x) + 'px';
+				} else {
+					toggleMin.style.left = 20 + 'px';
+				};
+				if ((toggleMin.offsetLeft - shift.x) >= toggleMax.offsetLeft){
+					toggleMin.style.left = (toggleMax.offsetLeft - 1) + 'px';
+				};			
+				scale.style.paddingLeft = (toggleMin.offsetLeft - 20) + 'px';
+				 minPrice.value = (toggleMin.offsetLeft - 20)*167;
+
 		};
 
 		var onMouseUp = function (mouseUp){
 			mouseUp.preventDefault();
-			document.removeEventListener('mousemove', mouseDownOn);
+			document.removeEventListener('mousemove', mouseDownOnMintoggle);
 			document.removeEventListener('mousemove', onMouseMove);
 		}
 
@@ -100,4 +113,70 @@ var mouseDownOn = function (button) {
 	});
 };
 
-mouseDownOn (toggleMin);
+var mouseDownOnMaxtoggle = function (button) {
+	button.addEventListener('mousedown', function (evtDown){
+		evtDown.preventDefault();
+		var startCoord = {
+			x: evtDown.clientX
+		};
+		var onMouseMove = function (moveEvt){
+			var shift = {
+				x: startCoord.x - moveEvt.clientX
+			};
+
+			startCoord = {
+				x: moveEvt.clientX
+			};
+				if ((toggleMax.offsetLeft - shift.x) >= 180){
+					toggleMax.style.left = 180 + 'px';
+				} else {
+					toggleMax.style.left = (toggleMax.offsetLeft - shift.x) + 'px';
+				};
+				if ((toggleMax.offsetLeft - shift.x) <= toggleMin.offsetLeft){
+					toggleMax.style.left = (toggleMin.offsetLeft + 1) + 'px';
+				};
+				scale.style.paddingRight = (182 - toggleMax.offsetLeft) + 'px';
+				maxPrice.value = Math.ceil(toggleMax.offsetLeft*166.6666666666);
+		};
+
+		var onMouseUp = function (mouseUp){
+			mouseUp.preventDefault();
+			document.removeEventListener('mousemove', mouseDownOnMaxtoggle);
+			document.removeEventListener('mousemove', onMouseMove);
+		}
+
+		document.addEventListener('mousemove', onMouseMove);
+		document.addEventListener('mouseup', onMouseUp);
+	});
+};
+
+
+mouseDownOnMintoggle (toggleMin);
+mouseDownOnMaxtoggle (toggleMax);
+
+maxPrice.addEventListener('change', function (evt){
+	evt.preventDefault();
+	if (maxPrice.value <= 30000){
+		toggleMax.style.left = (maxPrice.value/166.666) + 'px';
+		scale.style.paddingRight = (182 - toggleMax.offsetLeft) + 'px';
+
+	}	if (maxPrice.value <= minPrice.value) {
+		maxPrice.value = minPrice.value;
+		toggleMax.style.left = (maxPrice.value/166.666) + 'px';
+		scale.style.paddingRight = (182 - toggleMax.offsetLeft) + 'px';
+	} 
+});
+
+minPrice.addEventListener ('change', function(evt){
+	evt.preventDefault();
+	if (minPrice.value >= 0){
+		toggleMin.style.left = (minPrice.value/167) + 20 + 'px';
+		scale.style.paddingLeft = (toggleMin.offsetLeft - 20) + 'px';
+	}
+	
+	if (minPrice.value > maxPrice.value){
+		minPrice.value = maxPrice.value;
+		toggleMin.style.left = (minPrice.value/167) + 20 + 'px';
+		scale.style.paddingLeft = (toggleMin.offsetLeft - 20) + 'px';
+	}
+});
